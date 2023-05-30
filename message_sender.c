755ea50cd0a469da0186c80fc9c840e6594b6b1a
AlonPolsky <alonpolski@mail.tau.ac.il>
2023-05-30 18:00:17 +0300

    idk

--- message_sender.c
+++ message_sender.c
@@ -7,7 +7,6 @@
 #include "message_slot.h"
 
 #define ARGV_ARGUMENTS 3 + 1
-#define WRONG_ARG_AMOUNT_ERR "Wrong argumet's number."
 #define SYSCALL_EXIT_FAILURE -1
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
@@ -20,17 +19,17 @@ int main(int argc, char* argv[])
 {
     int fd;
 
-    ERROR_CHECK_SENDER(argc != ARGV_ARGUMENTS, WRONG_ARG_AMOUNT_ERR)
+    ERROR_CHECK_SENDER(argc != ARGV_ARGUMENTS, "Wrong argumet's number.\n")
     
     fd = open(argv[PATH_LOC], O_WRONLY);
 
-    ERROR_CHECK_SENDER(fd == SYSCALL_EXIT_FAILURE,"")
+    ERROR_CHECK_SENDER(fd == SYSCALL_EXIT_FAILURE,"Error on open.\n")
 
-    ERROR_CHECK_SENDER(ioctl(fd, MSG_SLOT_CHANNEL, atol(argv[CHANNEL_ID_LOC])) == SYSCALL_EXIT_FAILURE,"")
+    ERROR_CHECK_SENDER(ioctl(fd, MSG_SLOT_CHANNEL, atol(argv[CHANNEL_ID_LOC])) == SYSCALL_EXIT_FAILURE,"Error on ioctl.\n")
 
-    ERROR_CHECK_SENDER(write(fd,argv[MSG_LOC],strlen(argv[MSG_LOC])) == SYSCALL_EXIT_FAILURE,"")
+    ERROR_CHECK_SENDER(write(fd, argv[MSG_LOC], strlen(argv[MSG_LOC])) == SYSCALL_EXIT_FAILURE,"Error on write.\n")
 
-    ERROR_CHECK_SENDER(close(fd) == SYSCALL_EXIT_FAILURE,"")
+    ERROR_CHECK_SENDER(close(fd) == SYSCALL_EXIT_FAILURE,"Error on close.\n")
 
     exit(EXIT_SUCCESS);
 }
