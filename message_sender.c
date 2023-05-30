f6490e01300e098b0b7c37428e14a8bcb33478f8
AlonPolsky <alonpolski@mail.tau.ac.il>
2023-05-30 17:34:50 +0300

    idk

--- message_sender.c
+++ message_sender.c
@@ -12,29 +12,24 @@
 #define PATH_LOC 1
 #define CHANNEL_ID_LOC 2
 #define MSG_LOC
-#define ERROR_CHECK(condition, message){\
-    if(condition){\
-        perror(message);\
-        exit(EXIT_FAILURE);\
-    }\
-}
+#define ERROR_CHECK_SENDER(condition, message) ERROR_CHECK(condition, perror(condition); exit(EXIT_FAILURE);, EXIT_FAILURE)
 
 int main(int argc, char* argv[])
 {
     int fd;
     int channel;
 
-    ERROR_CHECK(argc != ARGV_ARGUMENTS, WRONG_ARG_AMOUNT_ERR)
+    ERROR_CHECK_SENDER(argc != ARGV_ARGUMENTS, WRONG_ARG_AMOUNT_ERR)
     
     fd = open(argv[PATH_LOC], O_WRONLY);
 
-    ERROR_CHECK(fd == SYSCALL_EXIT_FAILURE,)
+    ERROR_CHECK_SENDER(fd == SYSCALL_EXIT_FAILURE,"")
 
-    ERROR_CHECK(ioctl(fd, MSG_SLOT_CHANNEL, atol(argv[CHANNEL_ID_LOC])) == SYSCALL_EXIT_FAILURE,)
+    ERROR_CHECK_SENDER(ioctl(fd, MSG_SLOT_CHANNEL, atol(argv[CHANNEL_ID_LOC])) == SYSCALL_EXIT_FAILURE,"")
 
-    ERROR_CHECK(write(fd,argv[MSG_LOC],strlen(argv[MSG_LOC])) == SYSCALL_EXIT_FAILURE,)
+    ERROR_CHECK_SENDER(write(fd,argv[MSG_LOC],strlen(argv[MSG_LOC])) == SYSCALL_EXIT_FAILURE,"")
 
-    ERROR_CHECK(close(fd) == SYSCALL_EXIT_FAILURE,)
+    ERROR_CHECK_SENDER(close(fd) == SYSCALL_EXIT_FAILURE,"")
 
     exit(EXIT_SUCCESS);
 }
