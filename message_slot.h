8688639ec2c87f8e86c83b3792cd39bf0c0b75ed
AlonPolsky <alonpolski@mail.tau.ac.il>
2023-05-30 12:18:05 +0300

    idk

--- message_slot.h
+++ message_slot.h
@@ -9,19 +9,33 @@
 #include <linux/string.h>   /* for memset. NOTE - not string.h!*/
 #include <linux/slab.h>
 
-// The major device number.
-// We don't rely on dynamic registration
-// any more. We want ioctls to know this
-// number at compile time.
-//#define MAJOR_NUM 244
-#define MAJOR_NUM 236
+
+#define MAJOR_NUM 235
+
+#define DEVICE_NAME "msg_slot"
+#define BUF_LEN 128
+#define MAX_MSGSLOTS 256
+// It may be needed to have more than one File name.
+#define DEVICE_FILE_NAME "msg_slot_"
+#define MAX_CHANNELS 1 < 20
+#define SUCCESS 0
+#define FAIL -1
+#define FREE_CHANNEL 0 // We use that number to indicate free channel and illigal channel numbers because it's an illigal channel number.
+#define ILLEGAL_INDX -1
+#define MSG_SLOT_CHANNEL _IO(MAJOR_NUM, 0, unsigned long)
+#define MIN_WRITE 1
+#define CHANNEL_INDX ((file_data*)file->private_data)->channel_indx
+#define MINOR ((file_data*)file->private_data)->minor
+#define ERROR_CHECK(condition, extra_code, errno_value){\
+    if(condition){\
+        extra_code;\
+        return errno_value;\
+    }\
+}
 
 // Set the message of the device driver
 #define IOCTL_SET_ENC _IOW(MAJOR_NUM, 0, unsigned long)
 
 #define DEVICE_RANGE_NAME "char_dev"
-#define BUF_LEN 80
-#define DEVICE_FILE_NAME "simple_char_dev"
-#define SUCCESS 0
 
 #endif
