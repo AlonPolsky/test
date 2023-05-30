d808992a60d38b231a448cbfe787ee8777f4848b
AlonPolsky <alonpolski@mail.tau.ac.il>
2023-05-30 20:08:57 +0300

    almost

--- message_sender.c
+++ message_sender.c
@@ -8,25 +8,25 @@
 
 #define ARGV_ARGUMENTS 3 + 1
 
-#define RROR_CHECK_USERLEVEL(condition, message) ERROR_CHECK(condition, perror(message); exit(EXIT_FAILURE);, EXIT_FAILURE)
+#define ERROR_CHECK_USERLEVEL(condition, message) ERROR_CHECK(condition, perror(message); exit(EXIT_FAILURE);, EXIT_FAILURE)
 
 int main(int argc, char* argv[])
 {
     int fd;
 
-    RROR_CHECK_USERLEVEL(argc != ARGV_ARGUMENTS, WRONG_ARG_NUM_ERR)
+    ERROR_CHECK_USERLEVEL(argc != ARGV_ARGUMENTS, WRONG_ARG_NUM_ERR)
     
     fd = open(argv[PATH_LOC], O_WRONLY);
 
-    RROR_CHECK_USERLEVEL(fd == SYSCALL_EXIT_FAILURE,OPEN_ERR)
+    ERROR_CHECK_USERLEVEL(fd == SYSCALL_EXIT_FAILURE,OPEN_ERR)
 
-    RROR_CHECK_USERLEVEL(ioctl(fd, MSG_SLOT_CHANNEL, atol(argv[CHANNEL_ID_LOC])) == SYSCALL_EXIT_FAILURE, IOCTL_ERR)
+    ERROR_CHECK_USERLEVEL(ioctl(fd, MSG_SLOT_CHANNEL, atol(argv[CHANNEL_ID_LOC])) == SYSCALL_EXIT_FAILURE, IOCTL_ERR)
 
     printf("%zu \n", write(fd, argv[MSG_LOC], strlen(argv[MSG_LOC])));
 
-    RROR_CHECK_USERLEVEL(write(fd, argv[MSG_LOC], strlen(argv[MSG_LOC])) == SYSCALL_EXIT_FAILURE, WR_ERR)
+    ERROR_CHECK_USERLEVEL(write(fd, argv[MSG_LOC], strlen(argv[MSG_LOC])) == SYSCALL_EXIT_FAILURE, WR_ERR)
 
-    RROR_CHECK_USERLEVEL(close(fd) == SYSCALL_EXIT_FAILURE, CLOSE_ERR)
+    ERROR_CHECK_USERLEVEL(close(fd) == SYSCALL_EXIT_FAILURE, CLOSE_ERR)
 
     exit(EXIT_SUCCESS);
 }
