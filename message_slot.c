018ebf7b0cdd426fb100c3e49fa973dd32dff66e
AlonPolsky <alonpolski@mail.tau.ac.il>
2023-05-31 16:37:20 +0300

    idk

--- message_slot.c
+++ message_slot.c
@@ -131,7 +131,7 @@ static ssize_t device_read( struct file* file,
 //---------------------------------------------------------------
 
 static ssize_t device_write( struct file*       file,
-                             const void __user* buffer,
+                             const char __user* buffer,
                              size_t             length,
                              loff_t*            offset)
 {
