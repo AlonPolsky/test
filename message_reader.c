45e7a75130e417a89f2ec190fd30e75b5ee151da
AlonPolsky <alonpolski@mail.tau.ac.il>
2023-05-31 05:39:13 +0300

    idk

--- message_reader.c
+++ message_reader.c
@@ -7,7 +7,7 @@
 #include "message_slot.h"
 
 #define ARGV_ARGUMENTS 1 + 2
-#define ERROR_CHECK_USERLEVEL(syscall, message) ERROR_CHECK(syscall == SYSCALL_EXIT_FAILURE, perror(message); exit(EXIT_FAILURE);, EXIT_FAILURE)
+#define ERROR_CHECK_USERLEVEL(syscall, post_open, fd, message) ERROR_CHECK(syscall == SYSCALL_EXIT_FAILURE, perror(message); if(post_open){close(fd);} exit(EXIT_FAILURE);, EXIT_FAILURE)
 
 int main(int argc, char* argv[])
 {
@@ -25,15 +25,15 @@ int main(int argc, char* argv[])
     
     fd = open(argv[PATH_LOC], O_RDONLY);
 
-    ERROR_CHECK_USERLEVEL(fd, OPEN_ERR)
+    ERROR_CHECK_USERLEVEL(fd, 0, 0, OPEN_ERR)
 
-    ERROR_CHECK_USERLEVEL(ioctl(fd, MSG_SLOT_CHANNEL, atol(argv[CHANNEL_ID_LOC])), IOCTL_ERR)
+    ERROR_CHECK_USERLEVEL(ioctl(fd, MSG_SLOT_CHANNEL, atol(argv[CHANNEL_ID_LOC])), 1, fd, IOCTL_ERR)
 
-    ERROR_CHECK_USERLEVEL((len = read(fd, buf, BUF_LEN)), RD_ERR)
+    ERROR_CHECK_USERLEVEL((len = read(fd, buf, BUF_LEN)), 1, fd, RD_ERR)
 
-    ERROR_CHECK_USERLEVEL(close(fd), CLOSE_ERR)
+    ERROR_CHECK_USERLEVEL(close(fd), 0, 0, CLOSE_ERR)
 
-    ERROR_CHECK_USERLEVEL(write(STDOUT_FD, buf, len), WR_ERR)
+    ERROR_CHECK_USERLEVEL(write(STDOUT_FD, buf, len), 0, 0, WR_ERR)
 
 
     exit(EXIT_SUCCESS);
--- message_sender.c
+++ message_sender.c
@@ -8,7 +8,7 @@
 
 #define ARGV_ARGUMENTS 3 + 1
 
-#define ERROR_CHECK_USERLEVEL(syscall, message) ERROR_CHECK(syscall == SYSCALL_EXIT_FAILURE, perror(message); exit(EXIT_FAILURE);, EXIT_FAILURE)
+#define ERROR_CHECK_USERLEVEL(syscall, post_open, fd, message) ERROR_CHECK(syscall == SYSCALL_EXIT_FAILURE, perror(message); if(post_open){close(fd);} exit(EXIT_FAILURE);, EXIT_FAILURE)
 
 int main(int argc, char* argv[])
 {
@@ -24,13 +24,13 @@ int main(int argc, char* argv[])
     
     fd = open(argv[PATH_LOC], O_WRONLY);
 
-    ERROR_CHECK_USERLEVEL(fd,OPEN_ERR)
+    ERROR_CHECK_USERLEVEL(fd, 0, 0,OPEN_ERR)
 
-    ERROR_CHECK_USERLEVEL(ioctl(fd, MSG_SLOT_CHANNEL, atol(argv[CHANNEL_ID_LOC])), IOCTL_ERR)
+    ERROR_CHECK_USERLEVEL(ioctl(fd, MSG_SLOT_CHANNEL, atol(argv[CHANNEL_ID_LOC])), 1, fd, IOCTL_ERR)
 
-    ERROR_CHECK_USERLEVEL(write(fd, argv[MSG_LOC], strlen(argv[MSG_LOC])), WR_ERR)
+    ERROR_CHECK_USERLEVEL(write(fd, argv[MSG_LOC], strlen(argv[MSG_LOC])), 1, fd, WR_ERR)
 
-    ERROR_CHECK_USERLEVEL(close(fd), CLOSE_ERR)
+    ERROR_CHECK_USERLEVEL(close(fd), 1, fd, CLOSE_ERR)
 
     exit(EXIT_SUCCESS);
 }
