e2fbee992a4e58e86b2fa996fade8c4f3185df71
AlonPolsky <alonpolski@mail.tau.ac.il>
2023-05-31 13:45:21 +0300

    idk

---
+++ destroy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo rm /dev/message_slot_0
+sudo rm /dev/message_slot_1
+sudo rmmod message_slot
---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+make
+sudo mkmod message_slot.ko
+sudo mknod /dev/message_slot_0 c 235 0
+sudo mknod /dev/message_slot_1 c 235 1
+sudo chmod o+rw /dev/message_slot_0
+sudo chmod o+rw /dev/message_slot_1
+gcc -O3 -Wall -std=c11 message_sender.c -o message_sender
+gcc -O3 -Wall -std=c11 message_reader.c -o message_reader
